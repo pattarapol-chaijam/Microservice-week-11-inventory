893be8bc83a6788289e84282f8330a4e21001998
Pattarapol Chaijam <64160249@go.buu.ac.th>
2024-09-21 23:01:12 +0700

    ack and nack

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
+import { console } from 'inspector';
 
 @Controller()
 export class AppController {
@@ -8,5 +15,21 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @MessagePattern('order_created')
+  handleOrderCreated(@Payload() data: any, @Ctx() context: RmqContext) {
+    console.log(`Pattern: ${context.getPattern()}`);
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+    console.log('Order received for processing:', data);
+    const isInstock = true;
+    if (isInstock) {
+      console.log('Inventory available. Processing order.');
+      channel.ack(originalMessage);
+    } else {
+      console.log('Inventory not available.');
+      channel.nack(originalMessage);
+    }
   }
 }
